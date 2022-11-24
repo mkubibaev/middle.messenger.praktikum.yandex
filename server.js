886a1343c5f4af31eab0e6e11feb9d3243fc2c57
mkubibaev <mkubibaev@gmail.com>
2022-11-25 01:43:11 +0600

    try 17

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile('./dist/index.html');
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
